af14e664975c0fb8b4e6c02caa191d5ff55eb11c
KyleCampbell20 <elykcampbell1@gmail.com>
2022-03-22 16:44:53 -0400

    made changes

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['SECRET_KEY'] = 'md$bV+S*vG9-L'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mfvrdaqykxutvh:e937b9fef2be75a98e19cdcd24cc9b7994e8cd1fb96423958e3a1594cc130a47@ec2-44-194-92-192.compute-1.amazonaws.com:5432/d18dq6eqkos39b'
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
