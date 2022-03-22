d69660fe485b3aae2447a0bd3a22bae2c251cc19
KyleCampbell20 <elykcampbell1@gmail.com>
2022-03-22 14:54:30 -0400

    fixed databese  URI

--- app/config.py
+++ app/config.py
@@ -7,6 +7,6 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgres://mfvrdaqykxutvh:e937b9fef2be75a98e19cdcd24cc9b7994e8cd1fb96423958e3a1594cc130a47@ec2-44-194-92-192.compute-1.amazonaws.com:5432/d18dq6eqkos39b').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', 'app/static/uploads')
